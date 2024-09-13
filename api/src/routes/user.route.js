606bdec7451fe0a423c18cf41a6235f4c02649fd
Stanley Nwaegwu <kingstanley.ks16@gmail.com>
2024-09-13 18:49:03 +0100

    more information service structure and file handling for user storage in file system

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -3,8 +3,8 @@ const router = express.Router();
 
 const { signUpHandler, signInHandler } = require('../handler/user.handler.js')
 
-router.post('/signup', signUpHandler)
+router.post('/signup', signUpHandler);
 
-router.post('/signin', signInHandler)
+router.post('/signin', signInHandler);
 
 module.exports = router;
--- api/src/services/user.service.js
+++ api/src/services/user.service.js
@@ -1,7 +1,49 @@
+const { writeFileSync, appendFileSync, existsSync, readFileSync } = require('node:fs');
+const dbPath = './db/user.db'
 const signUp = async (body) => {
-    console.log('body ', body)
+    try {
+
+        body.id = (Math.random() * 10).toString(32).replace('.', '');
+        console.log('body ', body)
+        const exists = existsSync(dbPath)
+        console.log('exists: ', exists);
+        if (exists) {
+            // append to dbPath 
+            appendFileSync(dbPath, '\n' + JSON.stringify(body))
+        } else {
+            await writeFileSync(dbPath, JSON.stringify(body));
+            // console.log('write res: ', writeRes);
+        }
+        return body;
+    } catch (error) {
+        console.log('Signup error: ', error.message)
+    }
+}
+
+const signIn = async (body) => {
+    try {
+        const { email, password } = body;
+        const db = readFileSync(dbPath, 'UTF-8');
+        const users = db.split('\n');
+
+        console.log('Db Content: ', email);
+        let foundUser = null;
+        for (const user of users) {
+            const parsedUser = JSON.parse(JSON.stringify(user));
+            console.log('found user email: ', parsedUser.email)
+            if (Object.values(parsedUser).includes(email.toString())) {
+                console.log('parsed user: ', parsedUser)
+                console.log('user is found: ', parsedUser);
+                foundUser = parsedUser;
+                break;
+            }
+        }
+        return foundUser;
+    } catch (error) {
+        console.log('Error signing in: ', error)
+    }
 }
 
 module.exports = {
-    signUp
+    signUp, signIn
 }
